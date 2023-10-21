cc687e81ae5c710b57729470de80c55b65a428c2
mdrahed2023 <135752931+mdrahed2023@users.noreply.github.com>
2023-10-21 14:57:49 +0600

    m

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const productContainer = document.querySelector(".new_p_container");
 const productCards = document.querySelector(".product_cards");
 let themeBtn = document.querySelector(".top_bar__theme_icon");
 
+// select user menu cancel button 
+
+let usermenuCancel = document.querySelector(".user_menu__cancel");
+let userMenu = document.querySelector(".user");
+
 function addProductDiv() {
   if (productContainer.classList.contains("active")) {
     productContainer.classList.remove("active");
@@ -18,4 +23,12 @@ function addProductDiv() {
   
 }
 
-addProduct.addEventListener("click", addProductDiv);
+// addProduct.addEventListener("click", addProductDiv);
+
+usermenuCancel.addEventListener("click", () => {
+  usermenuCancel.parentElement.style.display = "none";
+})
+
+userMenu.addEventListener("click", function () {
+  usermenuCancel.parentElement.style.display = "block";
+})
